74f769dfd1479792826f420b90527c4bc8dc9292
Chibunud <ndubest56@gmail.com>
2023-12-30 15:17:54 +0100

    cookie-parser for user cookies

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const User = require('./models/user')
 const dotenv = require(`dotenv`).config()
 const bcrypt = require(`bcryptjs`)
 const jwt = require(`jsonwebtoken`)
+const cookieParser = require(`cookie-parser`)
 
 const app = express()
 
 app.use(express.json())
 app.use(cors({ origin: `http://localhost:5173`, credentials: true }))
+app.use(cookieParser())
 
 const bcryptSalt = bcrypt.genSaltSync(10)
 const jwtSecret = `kjhsj59479fjhfdkjjfhdkj29skkfjfjzfhuisrwe`
@@ -28,15 +30,32 @@ app.post(`/login`, async (req, res) => {
   if (userDoc) {
     const passOk = bcrypt.compareSync(password, userDoc.password)
     if (passOk) {
-      jwt.sign({ email: userDoc.email, id: userDoc._id }, jwtSecret, {}, (err, token) => {
-        if (err) throw err
-        res.cookie(`token`, token).json(userDoc)
-      })
+      jwt.sign(
+        { email: userDoc.email, id: userDoc._id, name: userDoc.name },
+        jwtSecret,
+        {},
+        (err, token) => {
+          if (err) throw err
+          res.cookie(`token`, token).json(userDoc)
+        }
+      )
     } else {
       res.status(422).json(`pass not ok`)
     }
   } else {
     res.tjson(`user not found`)
+  }
+})
+
+app.get(`/profile`, (req, res) => {
+  const { token } = req.cookies
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, (err, userDoc) => {
+      if (err) throw err
+      res.json(userDoc)
+    })
+  } else {
+    res.json(`no user`)
   }
 })
 
